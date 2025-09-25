13d4d8862a1803d7d7753f9d0757efe9503b1239
Reycko <78082869+Reycko@users.noreply.github.com>
2025-09-25 21:56:54 +0200

    feat: add stalker

---
+++ stalker/index.tsx
@@ -0,0 +1,130 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { findGroupChildrenByChildId, NavContextMenuPatchCallback } from "@api/ContextMenu";
+import { definePluginSettings } from "@api/Settings";
+import { Logger } from "@utils/Logger";
+import definePlugin, { OptionType } from "@utils/types";
+import { Menu } from "@webpack/common";
+import { UserContextProps } from "plugins/biggerStreamPreview";
+
+import * as status from "./status";
+import * as voice from "./voice";
+
+export const logger = new Logger("Stalker");
+export let targets: string[] = [];
+
+const parseTargets = (parse: string): string[] => {
+    const regex = /\s*(,?)\s*([0-9]+)/g;
+    const matches = [...parse.matchAll(regex)].map(value => {
+        return value.at(value.length - 1) as string;
+    });
+
+    targets = matches;
+
+    return matches;
+};
+
+export const settings = definePluginSettings({
+    stalkContext: {
+        type: OptionType.BOOLEAN,
+        default: true,
+        description: "Adds an option on the user context menu that enables stalking for users."
+    },
+
+    notifyCallJoin: {
+        type: OptionType.BOOLEAN,
+        default: true,
+        description: "Send a notification when a user joins a call.",
+    },
+
+    notifyOffline: {
+        type: OptionType.BOOLEAN,
+        default: true,
+        description: "Send a notification when a user goes offline."
+    },
+
+    notifyOnline: {
+        type: OptionType.BOOLEAN,
+        default: true,
+        description: "Send a notification when a user goes online.",
+    },
+
+    notifyDnd: {
+        type: OptionType.BOOLEAN,
+        default: true,
+        description: "Send a notification when a user goes on Do Not Disturb.",
+    },
+
+    notifyIdle: {
+        type: OptionType.BOOLEAN,
+        default: false,
+        description: "Send a notification when a user goes on idle.",
+    },
+
+    notifyGoOnline: {
+        type: OptionType.BOOLEAN,
+        default: true,
+        description: "Send a notification when a user logs onto Discord or leaves invisible, regardless of the 4 above options."
+    },
+
+    targets: {
+        type: OptionType.STRING,
+        placeholder: "1234,5678",
+        description: "List of user IDs to stalk, separate with a comma.",
+        default: "",
+        onChange: parseTargets,
+    },
+});
+
+const patchUserContext: NavContextMenuPatchCallback = (children, { user }: UserContextProps) => {
+    if (!settings.store.stalkContext) return;
+    if (!user) return;
+
+    const stalked = settings.store.targets.includes(user.id);
+    const group = findGroupChildrenByChildId("mark-channel-read", children) ?? children;
+
+    group.push(
+        <Menu.MenuItem
+            id="vc-st-stalk-user"
+            label={stalked ? "Stop Stalking User" : "Stalk User"}
+            action={() => {
+                if (stalked) {
+                    const { targets } = settings.store;
+                    settings.store.targets = settings.store.targets.replace(new RegExp(`(,?)(\\s*)(${user.id})`), "");
+                } else {
+                    settings.store.targets += `,${user.id}`;
+                    if (settings.store.targets.startsWith(",")) settings.store.targets = settings.store.targets.slice(1);
+                }
+
+                parseTargets(settings.store.targets);
+            }}
+        />
+    );
+};
+
+export default definePlugin({
+    name: "Stalker",
+    "description": "Notifies you whenever a person does something.",
+    authors: [{ name: "Reycko", id: 1123725368004726794n }],
+
+    contextMenus: {
+        "user-context": patchUserContext,
+    },
+
+    start() {
+        parseTargets(settings.store.targets);
+        status.init();
+        voice.init();
+    },
+
+    stop() {
+        status.deinit();
+        voice.deinit();
+    },
+
+    settings,
+});
---
+++ stalker/status.ts
@@ -0,0 +1,67 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { showNotification } from "@api/Notifications";
+import { ChannelStore, NavigationRouter, PresenceStore, SelectedChannelStore, UserStore } from "@webpack/common";
+
+import { logger, settings, targets } from ".";
+import { findByPropsLazy } from "@webpack";
+
+let lastStatuses: Statuses | undefined;
+
+type Statuses = { [id: string]: string; };
+
+export const init = () => {
+    PresenceStore.addChangeListener(statusChange);
+};
+
+export const deinit = () => {
+    PresenceStore.removeChangeListener(statusChange);
+    lastStatuses = {}; // clear lastStatuses to prevent notifs blah blah
+};
+
+export const statusChange = () => {
+    const rawNewStatuses: Statuses = PresenceStore.getState()?.statuses; // replace undefined with 'offline'
+    if (typeof rawNewStatuses !== "object") return;
+    const newStatuses: Statuses = Object.assign({}, rawNewStatuses);
+
+    for (const id of targets) {
+        if (!newStatuses[id]) newStatuses[id] = "offline";
+    }
+
+    if (!lastStatuses) lastStatuses = Object.assign({}, newStatuses); // we probably haven't init'd it yet, so let's do it
+
+    for (const [id, status] of Object.entries(newStatuses)) {
+        const isStalking = targets.includes(id);
+        const lastStatus = lastStatuses[id] ?? "offline";
+
+        if (isStalking && lastStatus !== status) {
+            let shouldNotify = false;
+            if (lastStatus === "offline" && settings.store.notifyGoOnline) shouldNotify = true;
+            if (status === "dnd" && settings.store.notifyDnd) shouldNotify = true;
+            if (status === "idle" && settings.store.notifyIdle) shouldNotify = true;
+            if (status === "online" && settings.store.notifyOnline) shouldNotify = true;
+            if (status === "offline" && settings.store.notifyOffline) shouldNotify = true;
+
+            if (lastStatus !== status && shouldNotify) {
+                const user = UserStore.getUser(id);
+                const color = `#${user.accentColor?.toString(16)}`;
+
+                showNotification({
+                    title: "Stalker",
+                    body: `${user.username} is now ${status === "dnd" ? "in " : ""}${status ?? "offline"}`,
+                    color,
+                    icon: user.getAvatarURL(),
+                    onClick: () => {
+                        NavigationRouter.transitionTo(`/channels/@me/${ChannelStore.getDMFromUserId(user.id)}`);
+                    },
+                });
+            }
+        }
+    }
+
+    lastStatuses = Object.assign({}, newStatuses);
+};
